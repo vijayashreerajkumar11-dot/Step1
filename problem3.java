0b18842dab36ce20fb1057fba74c0f70a77a5fdf
vijayashree <vijayashreerajkumar11@gmail.com>
2026-08-05 15:06:15 +0530

    Added traffic signal streak analyzer

---
+++ problem3.java
@@ -0,0 +1,38 @@
+public class problem3 {
+    // Method to find the longest streak
+    static void findLongestStreak(String signalLog) {
+
+        char longestColor = signalLog.charAt(0);
+        int longestLength = 1;
+
+        char currentColor = signalLog.charAt(0);
+        int currentLength = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == currentColor) {
+                currentLength++;
+            } else {
+                currentColor = signalLog.charAt(i);
+                currentLength = 1;
+            }
+
+            if (currentLength > longestLength) {
+                longestLength = currentLength;
+                longestColor = currentColor;
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestColor
+                + "' repeated " + longestLength + " times");
+    }
+
+    public static void main(String[] args) {
+
+        String signal1 = "RRGGGYRR";
+        findLongestStreak(signal1);
+
+        String signal2 = "RRRRYYGG";
+        findLongestStreak(signal2);
+    }
+}
